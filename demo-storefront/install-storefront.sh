a7a668ea5fab2312f5b4443ffec226a57b4c4aa6
Michael Bromley <michael@michaelbromley.co.uk>
2020-02-20 22:09:06 +0100

    Disable source map for storefront build
    Attempting to use less RAM so it works

--- demo-storefront/install-storefront.sh
+++ demo-storefront/install-storefront.sh
@@ -4,4 +4,4 @@ git clone https://github.com/vendure-ecommerce/storefront.git
 cd storefront
 git checkout 558545db3a2fa3012b4fa0f22412600e38f4c708
 yarn
-yarn build:ssr
+yarn build:ssr --sourceMap=false
