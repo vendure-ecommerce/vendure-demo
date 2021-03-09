3739bce49c15331bd974bbac4e4b0e358ed1ee94
Michael Bromley <michael@michaelbromley.co.uk>
2021-03-09 16:45:39 +0100

    Fix broken asset urls

--- src/plugins/landing-page/landing-page-plugin.ts
+++ src/plugins/landing-page/landing-page-plugin.ts
@@ -13,6 +13,7 @@ import path from 'path';
                 req.url.indexOf('/shop-api') !== 0 &&
                 req.url.indexOf('/admin') !== 0 &&
                 req.url.indexOf('/mailbox') !== 0 &&
+                req.url.indexOf('/assets') !== 0 &&
                 req.url.indexOf('/storefront') !== 0) {
                 const file = req.url === '/' ? 'index.html' : req.url;
                 res.sendFile(path.join(__dirname, '../../../static/landing-page', file));
