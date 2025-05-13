bd8edb879511856a4245916e3644ff93edcdcc46
David Höck <d.hoeck@elevantiq.com>
2025-05-13 09:35:55 +0200

    chore: Update to Vendure v3.3.0

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -1,88 +1,101 @@
-import {AdminUiPlugin} from '@vendure/admin-ui-plugin';
-import {AssetServerPlugin} from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from "@vendure/admin-ui-plugin";
+import { AssetServerPlugin } from "@vendure/asset-server-plugin";
 import {
-    createProxyHandler,
-    DefaultJobQueuePlugin,
-    DefaultSearchPlugin,
-    dummyPaymentHandler,
-    VendureConfig
-} from '@vendure/core';
-import {defaultEmailHandlers, EmailPlugin, FileBasedTemplateLoader} from '@vendure/email-plugin';
-import path from 'path';
-import {LandingPagePlugin} from './plugins/landing-page/landing-page-plugin';
-import {DemoModePlugin} from './plugins/demo-mode/demo-mode-plugin';
-import {HardenPlugin} from "@vendure/harden-plugin";
+  createProxyHandler,
+  DefaultJobQueuePlugin,
+  DefaultSchedulerPlugin,
+  DefaultSearchPlugin,
+  dummyPaymentHandler,
+  VendureConfig,
+} from "@vendure/core";
+import {
+  defaultEmailHandlers,
+  EmailPlugin,
+  FileBasedTemplateLoader,
+} from "@vendure/email-plugin";
+import path from "path";
+import { LandingPagePlugin } from "./plugins/landing-page/landing-page-plugin";
+import { DemoModePlugin } from "./plugins/demo-mode/demo-mode-plugin";
+import { HardenPlugin } from "@vendure/harden-plugin";
+import { SapErpIntegrationPlugin } from "./plugins/sap-erp-integration/sap-erp-integration.plugin";
 
 export const config: VendureConfig = {
-    apiOptions: {
-        port: 3000,
-        adminApiPath: 'admin-api',
-        shopApiPath: 'shop-api',
-        adminApiPlayground: {
-            settings: {'request.credentials': 'include'},
-        },
-        adminApiDebug: true,
-        shopApiPlayground: {
-            settings: {'request.credentials': 'include'},
-        },
-        shopApiDebug: true,
+  apiOptions: {
+    port: 3000,
+    adminApiPath: "admin-api",
+    shopApiPath: "shop-api",
+    adminApiPlayground: {
+      settings: { "request.credentials": "include" },
     },
-    authOptions: {
-        cookieOptions: {
-            secret: '9s8wl7vkd8',
-        },
-        requireVerification: true,
-        tokenMethod: ['cookie', 'bearer'],
-        superadminCredentials: {
-            identifier: process.env.SUPERADMIN_USERNAME ?? 'superadmin',
-            password: process.env.SUPERADMIN_PASSWORD ?? 'superadmin',
-        }
+    adminApiDebug: true,
+    shopApiPlayground: {
+      settings: { "request.credentials": "include" },
     },
-    dbConnectionOptions: {
-        type: 'better-sqlite3',
-        synchronize: false,
-        logging: false,
-        database: path.join(__dirname, '../vendure.sqlite'),
+    shopApiDebug: true,
+  },
+  authOptions: {
+    cookieOptions: {
+      secret: "9s8wl7vkd8",
     },
-    paymentOptions: {
-        paymentMethodHandlers: [dummyPaymentHandler],
+    requireVerification: true,
+    tokenMethod: ["cookie", "bearer"],
+    superadminCredentials: {
+      identifier: process.env.SUPERADMIN_USERNAME ?? "superadmin",
+      password: process.env.SUPERADMIN_PASSWORD ?? "superadmin",
     },
-    customFields: {},
-    plugins: [
-        DefaultJobQueuePlugin,
-        AssetServerPlugin.init({
-            route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
-            assetUrlPrefix: 'https://demo.vendure.io/assets/'
-        }),
-        EmailPlugin.init({
-            route: 'mailbox',
-            handlers: defaultEmailHandlers,
-            templateLoader: new FileBasedTemplateLoader(path.join(__dirname, '../static/email/templates')),
-            outputPath: path.join(__dirname, '../static/email/output'),
-            globalTemplateVars: {
-                fromAddress: '"Vendure Demo Store" <noreply@vendure.io>',
-                verifyEmailAddressUrl: 'https://demo.vendure.io/storefront/account/verify',
-                passwordResetUrl: 'https://demo.vendure.io/storefront/account/reset-password',
-                changeEmailAddressUrl: 'https://demo.vendure.io/storefront/account/change-email-address'
-            },
-            devMode: true,
-        }),
-        DefaultSearchPlugin,
-        AdminUiPlugin.init({
-            route: 'admin',
-            port: 3002,
-            adminUiConfig: {
-                apiHost: 'auto',
-                apiPort: 'auto',
-            },
-        }),
-        LandingPagePlugin,
-        DemoModePlugin,
-        HardenPlugin.init({
-            maxQueryComplexity: 10000,
-            hideFieldSuggestions: false,
-            apiMode: 'dev',
-        }),
-    ],
+  },
+  dbConnectionOptions: {
+    type: "better-sqlite3",
+    synchronize: false,
+    logging: false,
+    database: path.join(__dirname, "../vendure.sqlite"),
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [dummyPaymentHandler],
+  },
+  customFields: {},
+  plugins: [
+    DefaultJobQueuePlugin,
+    AssetServerPlugin.init({
+      route: "assets",
+      assetUploadDir: path.join(__dirname, "../static/assets"),
+      assetUrlPrefix: "https://demo.vendure.io/assets/",
+    }),
+    EmailPlugin.init({
+      route: "mailbox",
+      handlers: defaultEmailHandlers,
+      templateLoader: new FileBasedTemplateLoader(
+        path.join(__dirname, "../static/email/templates")
+      ),
+      outputPath: path.join(__dirname, "../static/email/output"),
+      globalTemplateVars: {
+        fromAddress: '"Vendure Demo Store" <noreply@vendure.io>',
+        verifyEmailAddressUrl:
+          "https://demo.vendure.io/storefront/account/verify",
+        passwordResetUrl:
+          "https://demo.vendure.io/storefront/account/reset-password",
+        changeEmailAddressUrl:
+          "https://demo.vendure.io/storefront/account/change-email-address",
+      },
+      devMode: true,
+    }),
+    DefaultSearchPlugin,
+    DefaultSchedulerPlugin.init({}),
+    AdminUiPlugin.init({
+      route: "admin",
+      port: 3002,
+      adminUiConfig: {
+        apiHost: "auto",
+        apiPort: "auto",
+      },
+    }),
+    LandingPagePlugin,
+    DemoModePlugin,
+    HardenPlugin.init({
+      maxQueryComplexity: 10000,
+      hideFieldSuggestions: false,
+      apiMode: "dev",
+    }),
+    SapErpIntegrationPlugin.init({}),
+  ],
 };
